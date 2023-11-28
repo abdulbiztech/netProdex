57a16db0d23c5e29151f919038f333a7fd8737eb
abdulbiztech <abdulbiztech@gmail.com>
2023-11-28 18:52:22 +0530

    some changes by abdul Tuesday

--- App.tsx
+++ App.tsx
@@ -1,76 +1,3 @@
-// import { StyleSheet, Text, View, Pressable } from 'react-native';
-
-// import {
-// 	WalletConnectModal,
-// 	useWalletConnectModal,
-// } from '@walletconnect/modal-react-native';
-// // Add in the useWalletConnectModal hook
-
-// const projectId = '305b48a4cda3e787729533faab2e38aa';
-
-// const providerMetadata = {
-// 	name: 'YOUR_PROJECT_NAME',
-// 	description: 'YOUR_PROJECT_DESCRIPTION',
-// 	url: 'https://your-project-website.com/',
-// 	icons: ['https://your-project-logo.com/'],
-// 	redirect: {
-// 		native: 'YOUR_APP_SCHEME://',
-// 		universal: 'YOUR_APP_UNIVERSAL_LINK.com',
-// 	},
-// };
-
-// export default function App() {
-// 	// Add in the useWalletConnectModal hook + props
-// 	const { open, isConnected, address, provider } = useWalletConnectModal();
-
-// 	// Function to handle the
-// 	const handleButtonPress = async () => {
-// 		if (isConnected) {
-// 			return provider?.disconnect();
-// 		}
-// 		return open();
-// 	};
-
-// 	// Main UI Render
-// 	return (
-// 		<View style={styles.container}>
-// 			<Text style={styles.heading}>NetProDex Wallet!</Text>
-// 			<Text>{isConnected ? address : 'No Connected'}</Text>
-// 			<Pressable
-// 				onPress={handleButtonPress}
-// 				style={styles.pressableMargin}
-// 			>
-// 				<Text>{isConnected ? 'Disconnect' : 'Connect'}</Text>
-// 			</Pressable>
-
-// 			<WalletConnectModal
-// 				explorerRecommendedWalletIds={[
-// 					'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
-// 				]}
-// 				explorerExcludedWalletIds={'ALL'}
-// 				projectId={projectId}
-// 				providerMetadata={providerMetadata}
-// 			/>
-// 		</View>
-// 	);
-// }
-
-// const styles = StyleSheet.create({
-// 	container: {
-// 		flex: 1,
-// 		backgroundColor: '#fff',
-// 		alignItems: 'center',
-// 		justifyContent: 'center',
-// 	},
-// 	heading: {
-// 		fontSize: 18,
-// 		fontWeight: 'bold',
-// 		marginBottom: 16,
-// 	},
-// 	pressableMargin: {
-// 		marginTop: 16,
-// 	},
-// });
 
 // import React from 'react';
 // import { StyleSheet, View, Text, Pressable, Linking } from 'react-native';
@@ -107,51 +34,176 @@
 //     marginTop: 16,
 //   },
 // });
-import React, { useEffect } from 'react';
-import { StyleSheet, View, Text, Pressable, Linking } from 'react-native';
+
+// import React, { useEffect } from 'react';
+// import { StyleSheet, View, Text, Pressable, Linking } from 'react-native';
+
+// export default function App() {
+//   // useEffect(() => {
+//   //     if()
+//   // }, []);
+//   const handleConnectAndOpenDApp = async () => {
+//     try {
+//       // Connect MetaMask
+//       await handleOpenMetaMask();
+
+//       // Open your dApp URL after users have connected MetaMask
+//       await handleOpenDApp();
+//     } catch (error) {
+//       console.error('Error during MetaMask connection:');
+//     }
+//   };
+
+//   const handleOpenMetaMask = async () => {
+//     try {
+//       // Open MetaMask website for users to install or connect
+//       const metamaskUrl = 'https://metamask.io/';
+//       await Linking.openURL(metamaskUrl);
+//     } catch (error) {
+//       throw new Error('Failed to open MetaMask:');
+//     }
+//   };
+
+//   const handleOpenDApp = async () => {
+//     try {
+//       // Open your dApp URL after users have connected MetaMask
+//       const dAppUrl = 'https://www.decentrawood.com/';
+//       await Linking.openURL(dAppUrl);
+//     } catch (error) {
+//       console.error('Error during DApp opening:');
+//     }
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.heading}>React Native MetaMask Integration</Text>
+//       <Pressable onPress={handleConnectAndOpenDApp} style={styles.pressableMargin}>
+//         <Text>Connect MetaMask and Open DApp</Text>
+//       </Pressable>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   heading: {
+//     fontSize: 18,
+//     fontWeight: 'bold',
+//     marginBottom: 16,
+//   },
+//   pressableMargin: {
+//     marginTop: 16,
+//   },
+// });
+
+
+
+// import React from 'react';
+// import { StyleSheet, View, Text, Pressable, Linking, Alert } from 'react-native';
+
+// export default function App() {
+//   const handleOpenMetaMask = async () => {
+//     const metamaskUrl = 'https://metamask.io/';
+//     try {
+//       await Linking.openURL(metamaskUrl);
+//       console.log("inside metamask app");
+
+//     } catch (error) {
+//       console.error('Failed to open MetaMask:', error);
+//       Alert.alert('Error', 'Failed to open MetaMask. Please make sure MetaMask is installed.');
+//     }
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.heading}>React Native MetaMask Integration</Text>
+//       {/* Pressing this button will instruct the user to open MetaMask manually */}
+//       <Pressable onPress={handleOpenMetaMask} style={styles.pressableMargin}>
+//         <Text>Open MetaMask</Text>
+//       </Pressable>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   heading: {
+//     fontSize: 18,
+//     fontWeight: 'bold',
+//     marginBottom: 16,
+//   },
+//   pressableMargin: {
+//     marginTop: 16,
+//   },
+// });
+
+
+import React from 'react';
+import { StyleSheet, View, Text, Pressable, Linking, Alert } from 'react-native';
 
 export default function App() {
-  // useEffect(() => {
-  //     if()
-  // }, []);
-  const handleConnectAndOpenDApp = async () => {
-    try {
-      // Connect MetaMask
-      await handleOpenMetaMask();
-
-      // Open your dApp URL after users have connected MetaMask
-      await handleOpenDApp();
-    } catch (error) {
-      console.error('Error during MetaMask connection:');
-    }
-  };
+  const metamaskUrl = 'https://metamask.io/';
+  const dAppUrl = 'https://netprodex.com/';
+  const appLink = "https://play.google.com/store/apps/details?id=io.metamask" // Replace with your desired link
 
   const handleOpenMetaMask = async () => {
     try {
-      // Open MetaMask website for users to install or connect
-      const metamaskUrl = 'https://metamask.io/';
-      await Linking.openURL(metamaskUrl);
+      const canOpen = await Linking.canOpenURL(metamaskUrl);
+
+      if (canOpen) {
+        await Linking.openURL(metamaskUrl);
+      } else {
+
+        await Alert.alert(
+          'MetaMask Not Installed',
+          'Please install MetaMask to proceed.',
+          [
+            {
+              text: 'Cancel',
+              style: 'cancel',
+            },
+            {
+              text: 'OK',
+              onPress: () => console.log('Open In Playstore',Linking.openURL(appLink)),
+            },
+          ],
+          { cancelable: false }
+        );
+      }
     } catch (error) {
-      throw new Error('Failed to open MetaMask:');
+      console.error('Failed to open MetaMask:', error);
     }
   };
 
-  const handleOpenDApp = async () => {
+  const handleOpenDAppInMetaMask = async () => {
     try {
-      // Open your dApp URL after users have connected MetaMask
-      const dAppUrl = 'https://www.decentrawood.com/';
       await Linking.openURL(dAppUrl);
     } catch (error) {
-      console.error('Error during DApp opening:');
+      console.error('Failed to open DApp in MetaMask browser:', error);
     }
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>React Native MetaMask Integration</Text>
-      <Pressable onPress={handleConnectAndOpenDApp} style={styles.pressableMargin}>
-        <Text>Connect MetaMask and Open DApp</Text>
-      </Pressable>
+      <Text style={styles.heading}>Net<Text style={styles.headingInside}>ProDex</Text> <Text style={styles.headingInside}>SW</Text>AP</Text>
+      <View style={styles.secondContainer}>
+        <Pressable onPress={handleOpenMetaMask} style={styles.pressableMargin}>
+          <Text style={styles.linkText}>Open MetaMask</Text>
+        </Pressable>
+        <Pressable onPress={handleOpenDAppInMetaMask} style={styles.pressableMargin}>
+          <Text style={styles.linkText}>Open Link</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -159,16 +211,42 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00090c',
     alignItems: 'center',
     justifyContent: 'center',
   },
   heading: {
-    fontSize: 18,
+    fontSize: 30,
+    textAlign: "center",
     fontWeight: 'bold',
     marginBottom: 16,
+    color: "#fff",
+    borderWidth: 3,
   },
   pressableMargin: {
-    marginTop: 16,
+    padding: 15,
+    color: "#fff",
+    backgroundColor: "#4bc2ec",
+    borderRadius: 7,
+    textAlign: "center",
+    width: 165,
   },
+
+  linkText: {
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 18,
+  },
+  secondContainer: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 15,
+    backgroundColor: "#031823",
+    padding: 40,
+
+  },
+  headingInside: {
+    color: "#4bc2ec",
+  }
 });
+
